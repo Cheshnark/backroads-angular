f379eadaae91ee86d9f63c446f290a2713d71291
Cheshnark <alejandrorubiosuela@gmail.com>
2024-01-26 13:42:04 +0100

    Create faq and profile pages. Update router. Update footer and nav-bar components.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,53 @@ import { LocationComponent } from './location/location.component';
 import { MapComponent } from './map/map.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { FaqComponent } from './faq/faq.component';
+import { ProfileComponent } from './profile/profile.component';
 
 export const routes: Routes = [
-  {path: 'honor-code', component: HonorCodeComponent},
-  {path: 'location', component: LocationComponent},
-  {path: 'map', component: MapComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {
+    path: 'honor-code',
+    title: 'Honor Code',
+    component: HonorCodeComponent
+  },
+  {
+    path: 'location/:id',
+    title: 'Location',
+    component: LocationComponent
+  },
+  {
+    path: 'map',
+    title: 'Map',
+    component: MapComponent
+  },
+  {
+    path: 'faq',
+    title: 'FAQ',
+    component: FaqComponent
+  },
+  {
+    path: 'profile',
+    title: 'Profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'login',
+    title: 'Login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    title: 'Register',
+    component: RegisterComponent
+  },
+  { 
+    path: '**',
+    component: NotFoundComponent
+  }
+  // {
+  //   path: '',
+  //   redirectTo:'login',
+  //   pathMatch: 'full'
+  // }
 ];
